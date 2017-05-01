01be69627e8e521912dbc60fea71c9fe0166706b
maximborodin <borodin.ms@phystech.edu>
2017-05-01 13:41:38 +0300

    Yandex maps. Yo

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var findMeButton = $('.find-me');
+ymaps.ready(init);
 
-// Check if the browser has support for the Geolocation API
-if (!navigator.geolocation) {
-
-  findMeButton.addClass("disabled");
-  $('.no-browser-support').addClass("visible");
-
-} else {
-
-  findMeButton.on('click', function(e) {
-
-    e.preventDefault();
-
-    navigator.geolocation.getCurrentPosition(function(position) {
-
-      // Get the coordinates of the current possition.
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-
-      $('.latitude').text(lat.toFixed(3));
-      $('.longitude').text(lng.toFixed(3));
-      $('.coordinates').addClass('visible');
-
-      // Create a new map and place a marker at the device location.
-      var map = new GMaps({
-        el: '#map',
-        lat: lat,
-        lng: lng
-      });
-
-      map.addMarker({
-        lat: lat,
-        lng: lng
-      });
-
+function init() {
+  var geolocation = ymaps.geolocation,
+    myMap = new ymaps.Map('map', {
+      center: [55, 34],
+      zoom: 10
+    }, {
+      searchControlProvider: 'yandex#search'
     });
 
+  // Сравним положение, вычисленное по ip пользователя и
+  // положение, вычисленное средствами браузера.
+  geolocation.get({
+    provider: 'yandex',
+    mapStateAutoApply: true
+  }).then(function (result) {
+    // Красным цветом пометим положение, вычисленное через ip.
+    result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+    result.geoObjects.get(0).properties.set({
+      balloonContentBody: 'Мое местоположение'
+    });
+    myMap.geoObjects.add(result.geoObjects);
   });
 
+  geolocation.get({
+    provider: 'browser',
+    mapStateAutoApply: true
+  }).then(function (result) {
+    // Синим цветом пометим положение, полученное через браузер.
+    // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
+    result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
+    myMap.geoObjects.add(result.geoObjects);
+  });
 }
